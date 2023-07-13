a13205975e45a33844252e462116c76ac38c86e8
Mangesh Dalvi <mangeshdalvi159915@gmail.com>
2023-07-14 00:14:21 +0530

    first commit

---
+++ admin/includes/dbconnection.php
@@ -0,0 +1,12 @@
+<?php
+$server ="localhost";
+$username ="root";
+$password ="";
+$dbname ="mdparlour";
+
+$con=mysqli_connect("localhost", "root", "", "mdparlour");
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+
+  ?>
---
+++ admin/includes/footer.php
@@ -0,0 +1,5 @@
+<!--footer-->
+    <div class="footer">
+       <p>&copy; 2022 Mangesh Dalvi Admin Panel.</p>
+    </div>
+        <!--//footer-->
